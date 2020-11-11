94b4a18fc28068e3e4da82a11e36008a98b5948a
Bennet <46792326+BennetE@users.noreply.github.com>
2020-11-11 18:24:31 +0100

    Update Main.java

--- src/main/java/de/bnder/taskmanager/main/Main.java
+++ src/main/java/de/bnder/taskmanager/main/Main.java
@@ -43,6 +43,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("########aaaaaaa################aaaaaaaaaaaaaaaa");
         System.out.println(System.getProperty("AUTHORIZATION_TOKEN"));
+        System.out.println(System.getenv("AUTHORIZATION_TOKEN"));
     }
 
     public static void maina(String[] args) {
