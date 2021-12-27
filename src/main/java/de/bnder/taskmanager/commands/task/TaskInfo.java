2aa4f6e7faa42268aa416179da8d95debe2afbb7
Jan Brinkmann <jan.brinkmann@bnder.de>
2021-12-28 00:21:42 +0100

    Update TaskInfo.java

--- src/main/java/de/bnder/taskmanager/commands/task/TaskInfo.java
+++ src/main/java/de/bnder/taskmanager/commands/task/TaskInfo.java
@@ -22,11 +22,11 @@ public class TaskInfo {
             if (task.exists()) {
                 final EmbedBuilder builder = new EmbedBuilder().setColor(Color.cyan).setTimestamp(Calendar.getInstance().toInstant()).setTitle(embedTitle + " - " + task.getId());
                 if (task.getStatus() == TaskStatus.TODO) {
-                    builder.addField(Localizations.getString("task_info_field_state", langCode), Localizations.getString("aufgaben_status_nicht_bearbeitet", langCode), true);
+                    builder.addField(Localizations.getString("task_info_field_state", langCode), Localizations.getString("task_status_to_do_keyword", langCode), true);
                 } else if (task.getStatus() == TaskStatus.IN_PROGRESS) {
-                    builder.addField(Localizations.getString("task_info_field_state", langCode), Localizations.getString("aufgaben_status_wird_bearbeitet", langCode), true);
+                    builder.addField(Localizations.getString("task_info_field_state", langCode), Localizations.getString("task_status_in_progress_keyword", langCode), true);
                 } else if (task.getStatus() == TaskStatus.DONE) {
-                    builder.addField(Localizations.getString("task_info_field_state", langCode), Localizations.getString("aufgaben_status_erledigt", langCode), true);
+                    builder.addField(Localizations.getString("task_info_field_state", langCode), Localizations.getString("task_status_done_keyword", langCode), true);
                 }
                 if (task.getDeadline() != null) {
                     builder.addField(Localizations.getString("task_info_field_deadline", langCode), task.getDeadline(), true);
